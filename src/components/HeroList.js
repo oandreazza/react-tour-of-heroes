44f9ae3c559b6758a02126a04d86e8058527cc99
Mauricio Sganderla <mauricio.webdev@gmail.com>
2017-06-23 21:20:29 +1000

    improved the way to delete hero applying a modal

--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -4,6 +4,8 @@ import { Grid, Row, Col} from 'react-bootstrap'
 import HeroCard from './HeroCard'
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 const styles = {
   buttonAction: {
@@ -15,13 +17,44 @@ const styles = {
 
 class HeroList extends React.Component{
 
+  state = {
+    dialog: false,
+    heroId: null
+  }
+
   componentWillMount = () => {
     this.props.getHeroes();
   }
 
+  deleteHeroModal = (heroId) => {
+    this.setState({dialog: true, heroId})
+  }
+
+  confirmDelete = () => {
+    this.props.deleteHero(this.state.heroId)
+    this.dismissModal()
+  }
+
+  dismissModal = () => {
+    this.setState({dialog:false})
+  }
+
   render() {
-    const {heroes, deleteHero} = this.props;
+    const {heroes} = this.props;
     const {buttonAction} = styles;
+    const modalActions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.dismissModal}
+      />,
+      <FlatButton
+          label="OK"
+          primary={true}
+          onClick={this.confirmDelete}
+      />
+    ]
+
     return(
       <div>
         <Link to="/heroes/add">
@@ -29,13 +62,22 @@ class HeroList extends React.Component{
             <ContentAdd />
           </FloatingActionButton>
         </Link>
+        <Dialog
+          title="Delete Hero"
+          actions={modalActions}
+          modal={false}
+          open={this.state.dialog}
+          onRequestClose={this.dismissModal}
+        >
+          Are you sure?
+        </Dialog>
         <Grid>
           <Row>
             {
               heroes.map( hero => {
                 return(
                   <Col md={4} xs={12} sm={6} style={{marginTop:50}}>
-                    <HeroCard key={hero.id} hero={hero} deleteHandle={deleteHero} />
+                    <HeroCard key={hero.id} hero={hero} deleteHandle={this.deleteHeroModal} />
                   </Col>
                 )
               })
